8138a4c1bcadd2c708cabef4c73b109a785f8ad7
mxve <68632137+mxve@users.noreply.github.com>
2023-10-16 17:15:43 +0200

    wip: get gitea repos

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,28 @@ mod config;
 
 #[allow(dead_code)]
 #[derive(Deserialize, Debug)]
-struct GHOwner {
+struct RepoOwner {
     login: String,
-    r#type: String,
 }
 
 #[allow(dead_code)]
 #[derive(Deserialize, Debug)]
-struct GHRepo {
+struct Repo {
     private: bool,
     fork: bool,
     clone_url: String,
     name: String,
     full_name: String,
-    owner: GHOwner,
+    owner: RepoOwner,
 }
 
-fn req_github(token: String, endpoint: String) -> String {
+#[derive(PartialEq)]
+enum RepoType {
+    Gitea,
+    GitHub,
+}
+
+fn get_github(token: String, endpoint: String) -> String {
     let endpoint = format!("https://api.github.com{}", endpoint);
     let mut buffer: Vec<u8> = Vec::new();
     match http_req::request::Request::new(&endpoint.as_str().try_into().unwrap())
@@ -30,29 +35,44 @@ fn req_github(token: String, endpoint: String) -> String {
         .header("X-GitHub-Api-Version", "2022-11-28")
         .send(&mut buffer)
     {
-        Ok(_) => {
-            let response = String::from_utf8(buffer).unwrap();
-            response
-        }
+        Ok(_) => String::from_utf8(buffer).unwrap(),
         Err(error) => panic!("Error: {:?}", error),
     }
 }
 
-fn req_github_repos(token: String, endpoint: String) -> Vec<GHRepo> {
+fn get_gitea(config: &config::CGitea, endpoint: &String) -> String {
+    let endpoint = format!("{}/api/v1{}", config.url, endpoint);
+    let mut buffer: Vec<u8> = Vec::new();
+    match http_req::request::Request::new(&endpoint.as_str().try_into().unwrap())
+        .header("User-Agent", "teahub")
+        .header("Accept", "application/json")
+        .header("Authorization", &format!("token {}", &config.token))
+        .send(&mut buffer)
+    {
+        Ok(_) => String::from_utf8(buffer).unwrap(),
+        Err(error) => panic!("Error: {:?}", error),
+    }
+}
+
+fn get_repos(config: &config::Config, endpoint: &String, repo_type: RepoType) -> Vec<Repo> {
     let mut page: u8 = 1;
-    let mut repos: Vec<GHRepo> = Vec::new();
+    let mut repos: Vec<Repo> = Vec::new();
 
     loop {
-        let endpoint = if endpoint.contains("?") {
-            format!("{}&per_page=100&page={}", endpoint, page)
+        let response = if repo_type == RepoType::GitHub {
+            let endpoint = if endpoint.contains('?') {
+                format!("{}&per_page=100&page={}", endpoint, page)
+            } else {
+                format!("{}?per_page=100&page={}", endpoint, page)
+            };
+            get_github(config.github.token.clone(), endpoint)
         } else {
-            format!("{}?per_page=100&page={}", endpoint, page)
+            get_gitea(&config.gitea, endpoint)
         };
-        let response = req_github(token.clone(), endpoint);
-        let mut page_repos: Vec<GHRepo> = serde_json::from_str(&response).unwrap();
+
+        let mut page_repos: Vec<Repo> = serde_json::from_str(&response).unwrap();
         let page_repos_cnt = page_repos.len(); // store repo count, value will be moved
 
-        println!("Page {} has {} repos", page, page_repos_cnt);
         repos.append(&mut page_repos);
         if page_repos_cnt < 100 {
             break;
@@ -63,16 +83,16 @@ fn req_github_repos(token: String, endpoint: String) -> Vec<GHRepo> {
     repos
 }
 
-fn get_github_repos(token: String, include_private: bool, include_starred: bool) -> Vec<GHRepo> {
-    let endpoint = if include_private {
+fn collect_github_repos(config: &config::Config) -> Vec<Repo> {
+    let endpoint = if config.github.include_private {
         "/user/repos"
     } else {
         "/user/repos?visibility=public"
     };
-    let mut repos = req_github_repos(token.clone(), endpoint.to_string());
+    let mut repos = get_repos(&config, &endpoint.to_string(), RepoType::GitHub);
 
-    if include_starred {
-        let mut starred = req_github_repos(token.clone(), "/user/starred".to_string());
+    if config.github.include_starred {
+        let mut starred = get_repos(&config, &"/user/starred".to_string(), RepoType::GitHub);
         repos.append(&mut starred);
     }
 
@@ -81,10 +101,8 @@ fn get_github_repos(token: String, include_private: bool, include_starred: bool)
 
 fn main() {
     let config = config::load_config(PathBuf::from("config.toml"));
-    let github_repos = get_github_repos(
-        config.github.token,
-        config.github.include_private,
-        config.github.include_starred,
-    );
-    println!("{:?}", github_repos);
+    let github_repos = collect_github_repos(&config);
+    println!("GH repos: {:?}", github_repos.len());
+    let gitea_repos = get_repos(&config, &"/user/repos".to_string(), RepoType::Gitea);
+    println!("GT repos: {:?}", gitea_repos.len());
 }
